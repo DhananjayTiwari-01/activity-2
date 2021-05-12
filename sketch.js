2b7c69c1c732dce9e0ff44d8b6431d0ffe0baa02
Dhananjay Tiwari <83721566+DhananjayTiwari-01@users.noreply.github.com>
2021-05-12 14:22:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,69 +1,32 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
 
-
-var engine , world ;
-var ball;
+var engine, world;
+var box1;
+var box2;
 var ground;
 
+function setup(){
+    var canvas = createCanvas(600,600);
+    engine = Engine.create();
+    world = engine.world;
 
-
-function setup() {
-  var canvas = createCanvas(800,400);
-
-engine = Engine.create();
-world  = engine.world
-
-var ball_option = {
-
-  restitution: 1.5
-}
-
-var ground_option = {
-
-  isStatic:true
-  
-  
+    box1 = new Box(200,300,50,50);
+    box2 = new Box(200,100,50,100);
+    
+    ground = new Ground(100,590,1000,10)
 
 }
 
-// ball = Bodies.circle(40,56,34,option);
-// World.add(world,ball);
+function draw(){
+    background(0);
+    Engine.update(engine);
 
-// ball2 = Bodies.circle(110,56,34,option);
-// World.add(world,ball2);
-
-ball = Bodies.circle(200,50,20,ball_option);
-World.add(world,ball);
+    box1.display();
+    box2.display();
 
 
-ground = Bodies.rectangle(200,390,200,20,ground_option);
-World.add(world,ground);
-
-
-
-
-}
-
-function draw() {
-  background(255,255,255);
-  Engine.update(engine);
-  
-  // ellipse(ball.position.x,ball.position.y,60,56);
-  // ellipse(ball2.position.x,ball2.position.y,60,56);
-
-  
-
-  rectMode(CENTER);
-  rect(ground.position.x,ground.position.y,400,20);
-
-  ellipseMode(RADIUS);
-  ellipse(ball.position.x,ball.position.y,20,20);
-
- 
-  
-
-  
-  drawSprites();
+    ground.display();
+   
 }
